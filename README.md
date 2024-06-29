06dd565e859d29c31942e6e821e856ebe3ccce55
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2024-06-29 19:54:12 +0300

    changed app icon

---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="https://www.flaticon.com/free-icons/studio" title="studio icons">Studio icons created by Freepik - Flaticon</a>
--- app/src/main/java/io/github/leonidius20/recorder/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/leonidius20/recorder/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import io.github.leonidius20.recorder.R
