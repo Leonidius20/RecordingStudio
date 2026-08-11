3203904aa667034136dc846697011823a3271d24
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2026-08-11 11:33:47 +0300

    fixed build error

--- app/src/main/java/io/github/leonidius20/recorder/ui/UiModule.kt
+++ app/src/main/java/io/github/leonidius20/recorder/ui/UiModule.kt
@@ -7,15 +7,23 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class UiModule {
 
+    @Binds
+    @Singleton
+    abstract fun bindStoreFactory(default: DefaultStoreFactory): StoreFactory
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MviModule {
+
     @Provides
     fun provideDefaultStoreFactory() = DefaultStoreFactory()
-
-    @Binds
-    abstract fun bindStoreFactory(default: DefaultStoreFactory): StoreFactory
 
 }
