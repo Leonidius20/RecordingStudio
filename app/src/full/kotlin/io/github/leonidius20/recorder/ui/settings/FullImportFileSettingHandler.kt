aacd21317dd2a706e6884a97e5134cb313919899
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2026-08-18 11:17:57 +0300

    fixed build for lite version (but it still builds native libs)

---
+++ app/src/full/kotlin/io/github/leonidius20/recorder/ui/settings/FullImportFileSettingHandler.kt
@@ -0,0 +1,77 @@
+package io.github.leonidius20.recorder.ui.settings
+
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
+import io.github.leonidius20.recorder.data.import_file.NativeBridge
+import io.github.leonidius20.recorder.data.recordings_list.RecordingsListRepository
+import javax.inject.Inject
+import javax.inject.Provider
+
+class FullImportFileSettingHandler @Inject constructor(
+    val nativeBridge: Provider<NativeBridge>,
+    val recordingsListRepository: RecordingsListRepository,
+) : ImportFileSettingHandler() {
+
+    lateinit var launcher: ActivityResultLauncher<Array<String>>
+
+    override fun launchImport() {
+        launcher.launch(arrayOf("audio/*"))
+    }
+
+    override fun register() = with(fragment) {
+        launcher =
+            registerForActivityResult(ActivityResultContracts.OpenDocument()) { selectedUri ->
+                if (selectedUri == null) return@registerForActivityResult
+                val resolver = requireContext().contentResolver
+                val mime = resolver.getType(selectedUri) ?: "audio/ogg"
+                val copyUri = recordingsListRepository.createRecordingFile("imported_file", mime)
+
+                val sourceFd = resolver.openFileDescriptor(selectedUri, "r")!!
+                val destFd = resolver.openFileDescriptor(copyUri, "w")!!
+
+                try {
+                    val result = nativeBridge.get().copyFile(sourceFd.fd, destFd.fd)
+                    Toast.makeText(
+                        requireContext(),
+                        if (result) "success" else "fail",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } catch (t: Throwable) {
+                    t.printStackTrace() // todo: replace with timber or crash reporting
+                    Toast.makeText(requireContext(), "fail (error)", Toast.LENGTH_SHORT).show()
+                } finally {
+                    sourceFd.close()
+                    destFd.close()
+                }
+
+                //val bytesCopied = resolver.openOutputStream(copyUri, "rw")!!.use { out ->
+                //    resolver.openInputStream(selectedUri)!!.use { input ->
+                //        input.copyTo(out)
+                //    }
+                //}
+                //resolver.update(copyUri, ContentValues().apply {
+                //    put(MediaStore.MediaColumns.SIZE, bytesCopied)
+                //}, null, null)
+
+            }
+    }
+
+}
+
+@Module
+@InstallIn(FragmentComponent::class)
+abstract class FullSettingsModule {
+
+    @FragmentScoped
+    @Binds
+    abstract fun bindImportHandler(
+        handler: FullImportFileSettingHandler,
+    ) : ImportFileSettingHandler
+
+}
--- app/src/main/kotlin/io/github/leonidius20/recorder/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/io/github/leonidius20/recorder/ui/settings/SettingsFragment.kt
@@ -4,20 +4,14 @@ import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.leonidius20.recorder.BuildConfig
 import io.github.leonidius20.recorder.R
-import io.github.leonidius20.recorder.data.import_file.NativeBridge
-import io.github.leonidius20.recorder.data.recordings_list.RecordingsListRepository
 import io.github.leonidius20.recorder.data.settings.Settings
 import javax.inject.Inject
-import javax.inject.Provider
 
 @AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
@@ -26,39 +20,11 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
     lateinit var settings: Settings
 
     @Inject
-    lateinit var recordingsListRepository: RecordingsListRepository // todo: remove
+    lateinit var importHandler: ImportFileSettingHandler
 
-    @Inject
-    lateinit var nativeBridge: Provider<NativeBridge>
-
-    val launchFileSelector = registerForActivityResult(ActivityResultContracts.OpenDocument()) { selectedUri ->
-        if (selectedUri == null) return@registerForActivityResult
-        val resolver = requireContext().contentResolver
-        val mime = resolver.getType(selectedUri) ?: "audio/ogg"
-        val copyUri = recordingsListRepository.createRecordingFile("imported_file", mime)
-
-        val sourceFd = resolver.openFileDescriptor(selectedUri, "r")!!
-        val destFd = resolver.openFileDescriptor(copyUri, "w")!!
-
-        try {
-            val result = nativeBridge.get().copyFile(sourceFd.fd, destFd.fd)
-            Toast.makeText(requireContext(), if (result) "success" else "fail", Toast.LENGTH_SHORT).show()
-        } catch (t: Throwable) {
-            t.printStackTrace() // todo: replace with timber or crash reporting
-            Toast.makeText(requireContext(), "fail (error)", Toast.LENGTH_SHORT).show()
-        } finally {
-            sourceFd.close()
-            destFd.close()
-        }
-
-        //val bytesCopied = resolver.openOutputStream(copyUri, "rw")!!.use { out ->
-        //    resolver.openInputStream(selectedUri)!!.use { input ->
-        //        input.copyTo(out)
-        //    }
-        //}
-        //resolver.update(copyUri, ContentValues().apply {
-        //    put(MediaStore.MediaColumns.SIZE, bytesCopied)
-        //}, null, null)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        importHandler.attach(this@SettingsFragment)
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -91,7 +57,7 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
             Preference(requireContext()).apply {
                 title = "Import file"
                 setOnPreferenceClickListener { _ ->
-                    launchFileSelector.launch(arrayOf("audio/*"))
+                    importHandler.launchImport()
                     true
                 }
                 preferenceScreen.addPreference(this)
