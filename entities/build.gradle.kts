c10dc69160776d5512878f5c4b7ca7901ff887a7
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2026-08-31 01:50:33 +0300

    changed target compatilibilty to be same across the board

--- entities/build.gradle.kts
+++ entities/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinJvm)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
     }
 }
 
